118a8019b99e7e5f24bf1524f1fd92a102435236
Linar Davletgareev <davletka@gmail.com>
2023-12-27 00:40:09 +0400

    errors fixed

--- test.py
+++ test.py
@@ -1,151 +1,178 @@
 from bs4 import BeautifulSoup
-from classes import Car
-import pandas as pd
-import datetime as dt
+import time
+import json
+import random
 from icecream import ic
-import re
+
 
 from get_html import get_html
-from main import read_config
+from time_diff import time_diff
+from url_to_df import url_to_df, df_refactor
+from df_to_db_sqlmodel import create_rows
+from classes import Car
 
-import logging
-logging.basicConfig(filename='description.log', encoding='utf-8', level=logging.INFO)
 
-
-def url_to_df(config: list, car: Car, page: int) -> 0 | 1:
-    url = f'https://www.avito.ru/all/avtomobili/{car.brand}/{car.model}?p={page}'
-    ic(url)
-
-    html = get_html(url)
-    try:
-        soup = BeautifulSoup(html, 'html.parser')
-    except TypeError as e:
-        print(e)
-        print(f'Page {page} was not parsed due to an error')
-        soup = None
-        
-    main_df = pd.DataFrame() # create main dataframe
+def read_config() -> list[str]:
+    '''read config file with avito css classes'''
+    with open("config.txt", "r", encoding="utf-8") as file:
+        return file.read().splitlines()
     
-    # parse headers
-    if soup:
-        headers = soup.find_all('h3')
+
+def sleep_time() -> None:
+    '''sleep for random time with seconds status bar'''
+    secs = random.randint(50, 80)
+    for _ in range(secs):
+        time.sleep(1)
+        print('-', end='', flush=True)
+    print(f' {secs}', end='', flush=True)
+    print('\n', end='')
+
+
+def create_car_objects() -> list:
+    '''read json file with brand and model names
+    create list with car objects with brand and model names'''
+    with open("cars.json", "r", encoding="utf-8") as file:
+        car_models = json.load(file)
+
+    car_objects = [Car(brand, model) for brand, models in car_models.items() for model in models]
+    return car_objects
+
+
+def calculate_remaining_time(time_a: int) -> None:
+    '''calculate remaining time after every processed page'''
+    global total_time
+    time_b = int(time.time())
+    time_per_page = time_b - time_a
+    total_time -= time_per_page
+    if total_time//60 > 59:
+        print(f"Estimated time remaining: {total_time//3600} h {total_time%3600//60} min\n")
     else:
-        return 0
+        print(f"Estimated time remaining: {total_time//60} min\n")
+
+
+def url_to_db(config: list[str], car: Car, page: int) -> None:
+    df = url_to_df(config, car, page)
+    df = df_refactor(df)
+    create_rows(df)
+
+
+def parse_car(car: object, car_counter: int, len_car_objects: int) -> None:
+    '''scrape a car object and return time left'''
+    global total_time
+
+    url = f"https://www.avito.ru/all/avtomobili/{car.brand}/{car.model}"
+    html_content = get_html(url)
+
+    try:
+        soup = BeautifulSoup(html_content, "html.parser")
+        pages = soup.find_all("span", class_="styles-module-text-InivV")
+        pages_num = int(pages[-1].text) # find number of pages of a car.model
+        pages_lst = list(range(1, pages_num + 1)) # create list of pages
+        random.shuffle(pages_lst) # shuffle list of pages
+    except Exception as e:
+        print(f"❌ {e}")
+        sleep_time()  # waiting
+        parse_car(car, car_counter, len_car_objects)
     
-    headers_list = list(map(lambda x: x.text, headers))
-    headers_list = headers_list[1:-1]
-    headers_list = list(map(lambda x: x.replace('\xa0', '').replace(' ', ''), headers_list))
+    sleep_time()  # waiting
+    # scrape each page and return total time left for calculation
+    parse_pages(car, car_counter, len_car_objects, pages_lst)
+
+
+def parse_pages(car: object, car_counter: int, len_car_objects: int, pages_lst: list) -> None:
+    '''parse each page of a car and return time left as total_time'''
+    global total_time
     
-    # create build_year column
-    main_df['build_year'] = list(map(lambda x: re.search(r',(\d{4})($|,)', x).group(1), headers_list))
-
-    # create mileage_kms column
-    for i in range(len(headers_list)):
-        if headers_list[i][-2:] != 'км':
-            headers_list[i] += ', 0км'
-        
-    main_df['mileage_kms'] = list(map(lambda x: re.search(r'(\d+)км', x).group(1), headers_list))
-
-    # parse prices
-    prices = soup.find_all('strong', class_=config[7])
-    prices_list = list(map(lambda x: x.text[:-2], prices))
-    prices_list = prices_list[1:]
-   
-    # remove 'от ' from a price
-    prices_list = list(map(lambda x: x.replace('от ', '').replace('\xa0', ''), prices_list))
-    
-    # create price_rub column
-    main_df['price_rub'] = prices_list
-    
-    # add date column
-    main_df['date'] = dt.datetime.today().strftime("%Y/%m/%d")
-    main_df['date'] = pd.to_datetime(main_df['date'])
-
-    # parse descriptions
-    descriptions = soup.find_all('p', class_=config[4])
-    descriptions_list = []
-    for description in descriptions:
-        if (description.text[0].isnumeric() or description.text.split(', ')[0] == 'Битый')\
-        and description.text.split(', ')[-1] != 'электро':
-            descriptions_list.append(description.text.replace('\xa0', ''))
-    prepandas_descriptions_list = list(map(lambda x: x.split(), descriptions_list))
-    
-    # insert 0 kms mileage to new cars
-    for i in prepandas_descriptions_list:
-        if len(i) == 6:
-            i.insert(0, '0')
-        if len(i[1]) == 1:
-            i[1] +='.0'
-
-    # add battered data
-    prepandas_descriptions_list = list(map(lambda x: x if x[0] == 'Битый,' else [0] + x, prepandas_descriptions_list))
-    for _ in prepandas_descriptions_list:
-        ic(len(_))
-        ic(_)
-        print('---')
-    
-    # create descriptions temp dataframe
-    descriptions_temp = pd.DataFrame(prepandas_descriptions_list)
-    descriptions_temp = descriptions_temp[[2, 3, 4, 6, 7, 0]]
-    descriptions_temp[4] = descriptions_temp[4].str.extract(r'(\d+)')
-    descriptions_temp[6] = descriptions_temp[6].str.rstrip(',')
-    
-    # add descriptions temp dataframe to dataframe
-    main_df[['engine', 'trans', 'horse_pwr', 'drive', 'gas', 'battered']] = descriptions_temp
-    
-    # add brand and model columns
-    main_df['brand'] = car.brand.title()
-    main_df['model'] = car.model.title().replace('_', ' ')
-    
-    # remove battered cars
-    main_df = main_df[main_df['battered'] == 0]
-
-    # rearranging columns order
-    main_df= main_df[['brand', 'model', 'engine', 'horse_pwr', 'trans', 'gas', 'drive',
-                        'build_year', 'mileage_kms', 'price_rub', 'date']]
-    return main_df
-
-def df_refactor(main_df: pd.DataFrame) -> pd.DataFrame:
-    # remove invalid data
-    main_df = main_df[~(main_df['engine'].astype(str).str.len() > 3)]
-    main_df.drop_duplicates(inplace=True)
-    main_df = main_df[~(main_df['horse_pwr'].isna())]
-    
-    # covert price_rub to integer
-    main_df['price_rub'] = main_df['price_rub'].astype('int32')
-
-    # convert mileage to integer
-    main_df['mileage_kms'] = main_df['mileage_kms'].astype('int32')
-
-    # convert buid_year to datetime
-    main_df['build_year'] = pd.to_datetime(main_df['build_year'], format='%Y')
-    main_df['build_year'] = main_df['build_year'].dt.year
-    
-    # convert pub_date to datetime
-    main_df['date'] = pd.to_datetime(main_df['date'], format='%Y-%m-%d')
-    main_df['date'] = main_df['date'].dt.date
-
-    # convert horse power to integer
-    main_df['horse_pwr'] = main_df['horse_pwr'].astype('int32')
-
-    # convert to categorical dtype
-    main_df['gas'] = main_df['gas'].astype('category')
-    main_df['drive'] = main_df['drive'].astype('category')
-    main_df['trans'] = main_df['trans'].astype('category')
-    main_df['brand'] = main_df['brand'].astype('category')
-    main_df['model'] = main_df['model'].astype('category')
-    main_df['engine'] = main_df['engine'].astype('float')
-
-    return main_df
-
-
-def main():
     config = read_config()
-    car = Car('audi', 'q8')
-    page = 7
-    main_df = url_to_df(config, car, page)
-    main_df = df_refactor(main_df)
-    ic(main_df)
+    
+    pages_num = len(pages_lst) # number of pages of a car.model
+    page_counter = 1 # count current page
+    retry_lst = [] # list of pages that were not parsed due to an error
+    
+    for page in pages_lst:
+        time_a = int(time.time())
+        
+        try:
+            url_to_db(config, car, page)
+            print(f"Car {car_counter}/{len_car_objects}, page {page_counter}/{pages_num} processed")
 
-if __name__ == '__main__':
+            sleep_time()  # waiting
+            
+            page_counter += 1
+        except Exception as e:
+            print(f"❌ {e}")
+            retry_lst.append((car, page))
+            sleep_time()  # waiting
+
+        # calculate remaining time
+        calculate_remaining_time(time_a)
+    
+    retry_parse_pages(retry_lst) # retry pages that were not parsed due to an error
+    
+    
+def retry_parse_pages(retry_lst: list) -> None:
+    '''retry pages that were not parsed due to an error'''
+    global total_time
+    internal_retry_lst = [] # copy list of pages that were not parsed due to an error
+    config = read_config()
+    for car, page in retry_lst:
+        time_a = int(time.time())
+        try:
+            print(f"Retry processing page {page}")
+            url_to_db(config, car, page)
+            sleep_time()  # waiting
+        except Exception as e:
+            print(f"❌ {e}")
+            internal_retry_lst.append((car, page))
+            sleep_time()  # waiting
+        calculate_remaining_time(time_a) # calculate remaining time
+        
+    if internal_retry_lst:
+        retry_parse_pages(internal_retry_lst)
+        
+        
+def retry_operation(operation, max_retries=3):
+    retries = 0
+    while retries < max_retries:
+        try:
+            result = operation()
+            return result  # Return the result if the operation succeeds
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            retries += 1
+            print(f"Retrying ({retries}/{max_retries})...")
+            sleep_time()
+    raise Exception(f"Operation failed after {max_retries} retries")
+
+
+def main() -> None:
+    objects_counter = 0
+    global total_time
+    car_objects = [Car('audi', 'q7')]
+
+    # load json file for storing timing
+    with open("cars.json", "r", encoding="utf-8") as file:
+        timing_data = json.load(file)
+
+    # calculate total time to execute loop
+    total_time = sum(map(lambda x: sum(x), (list(car.values()) for car in timing_data.values())))
+
+    for car_object in car_objects:
+        time_a = int(time.time()) # start timing
+        objects_counter += 1
+        parse_car(car_object, objects_counter, len(car_objects)) #total_time
+        time_b = int(time.time())
+        elapsed_time = time_b - time_a
+
+        # calculate remaining time
+        time_diff_sec, timing_data = time_diff(car_object, elapsed_time, timing_data)
+        total_time += time_diff_sec
+
+        # update cars.json
+        with open("cars.json", "w", encoding="utf-8") as file:
+            json.dump(timing_data, file, indent=4)
+    print("Well done!")
+
+
+if __name__ == "__main__":
     main()
